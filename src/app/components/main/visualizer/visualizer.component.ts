9a0f32c11991f57a550746a8bf2cb4224481c5b8
Sebastian Leichsenring <Sebastian.Leichsenri@aitag.com>
2022-09-06 21:49:50 +0200

    enhancements

--- src/app/components/main/visualizer/visualizer.component.ts
+++ src/app/components/main/visualizer/visualizer.component.ts
@@ -1,36 +1,32 @@
-import { AfterViewInit, Component, ElementRef, HostListener, OnDestroy, OnInit, ViewChild, ViewContainerRef } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  HostListener,
+  OnDestroy,
+  OnInit,
+  ViewChild,
+  ViewContainerRef,
+} from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { Store } from '@ngrx/store';
-import { BehaviorSubject, Subscription } from 'rxjs';
-import { filter, map, switchMap, take } from 'rxjs/operators';
+import { BehaviorSubject, Subscription, timer } from 'rxjs';
+import { filter, map, startWith, switchMap, take } from 'rxjs/operators';
 import { selectedGoodEdgeColor } from 'src/app/globals';
-import { showAnimation } from 'src/lib/shared/animations/show';
 import { NoSolutionDialogComponent } from '../../dialog/no-solution-dialog/no-solution-dialog.component';
 import { VisualizerComponentService } from './visualizer-component-service';
-
-import * as fromISolutionState from 'src/app/store/reducers/i-solution.reducers';
 import { selectCurrentSolution } from 'src/app/store/selectors/i-solution.selectors';
+import { fadeInAnimation } from 'src/lib/shared/animations/fade-in';
+import { selectSnapshot } from 'src/lib/process-builder/globals/select-snapshot';
 
 @Component({
   selector: 'app-visualizer',
   templateUrl: './visualizer.component.html',
   styleUrls: ['./visualizer.component.css'],
-  providers: [
-    VisualizerComponentService
-  ],
-  animations: [showAnimation]
+  providers: [VisualizerComponentService],
+  animations: [fadeInAnimation],
 })
 export class VisualizerComponent implements AfterViewInit, OnDestroy, OnInit {
-
-  public currentSolution$ = this._store.select(selectCurrentSolution);
-  public hasCurrentSolution$ = this.currentSolution$.pipe(map(solution => !!solution));
-
-  private _menuVisible = new BehaviorSubject<boolean>(true);
-  menuVisible$ = this._menuVisible.asObservable();
-
-  private _displayDetails = new BehaviorSubject<boolean>(true);
-  displayDetails$ = this._displayDetails.asObservable();
-
   @ViewChild('visualizerWrapper', { static: false }) set visualizerWrapperRef(ref: ElementRef<HTMLDivElement>) {
     if (!ref) {
       return;
@@ -38,35 +34,66 @@ export class VisualizerComponent implements AfterViewInit, OnDestroy, OnInit {
     this.visualizerComponentService.setVisualizerWrapper(ref);
   }
 
+  public currentSolution$ = this._store.select(selectCurrentSolution);
+  public hasCurrentSolution$ = this.currentSolution$.pipe(
+    map((solution) => !!solution)
+  );
+
+  private _menuVisible = new BehaviorSubject<boolean>(true);
+  private _userToggledMenu = new BehaviorSubject<boolean>(false);
+  menuVisible$ = this._menuVisible.asObservable();
+
+  private _displayDetails = new BehaviorSubject<boolean>(true);
+  displayDetails$ = this._displayDetails.asObservable();
+
+  displaySolution$ = this.menuVisible$.pipe(
+    switchMap(() => timer(500).pipe(map(() => true), startWith(false)))
+  );
+
   private _subscriptions = new Subscription();
 
   constructor(
     public visualizerComponentService: VisualizerComponentService,
     private _dialog: MatDialog,
     private _viewContainerRef: ViewContainerRef,
-    private _store: Store,
-  ) { }
+    private _store: Store
+  ) {}
 
-  ngAfterViewInit(): void {
+  public ngAfterViewInit(): void {
     this.validateClient();
   }
 
-  ngOnDestroy(): void {
+  public ngOnDestroy(): void {
     this._subscriptions.unsubscribe();
     this.visualizerComponentService.dispose();
   }
 
   ngOnInit(): void {
-    this._subscriptions.add(...[
-      this.visualizerComponentService.resized$.pipe(switchMap(() => this.visualizerComponentService.visualizerWrapper$.pipe(take(1)))).subscribe((ref: ElementRef<HTMLDivElement>) => {
-        this.visualizerComponentService.setSceneDimensions(ref.nativeElement.clientWidth, ref.nativeElement.clientHeight, true);
-      }),
+    this._subscriptions.add(
+      ...[
+        this.visualizerComponentService.resized$
+          .pipe(
+            switchMap(() =>
+              this.visualizerComponentService.visualizerWrapper$.pipe(take(1))
+            )
+          )
+          .subscribe((ref: ElementRef<HTMLDivElement>) => {
+            this.visualizerComponentService.setSceneDimensions(
+              ref.nativeElement.clientWidth,
+              ref.nativeElement.clientHeight,
+              true
+            );
+          }),
 
-      this.menuVisible$.subscribe(() => this.visualizerComponentService.triggerResizeEvent())
-    ]);
+        this.menuVisible$.subscribe(() =>
+          this.visualizerComponentService.triggerResizeEvent()
+        ),
+      ]
+    );
 
     this._subscriptions.add(
-      this.hasCurrentSolution$.pipe(filter(hasCurrentSolution => !hasCurrentSolution))
+      this.hasCurrentSolution$
+        .pipe(filter((hasCurrentSolution) => !hasCurrentSolution))
         .subscribe(() => this.showNoSolutionDialog())
     );
   }
@@ -75,25 +102,36 @@ export class VisualizerComponent implements AfterViewInit, OnDestroy, OnInit {
     this._dialog.open(NoSolutionDialogComponent, {
       panelClass: 'no-padding-dialog',
       disableClose: true,
-      viewContainerRef: this._viewContainerRef
+      viewContainerRef: this._viewContainerRef,
     });
   }
 
-  toggleMenu = () => this._menuVisible.next(!this._menuVisible.value);
+  async toggleMenu(){
+    const menuVisible = await selectSnapshot(this._menuVisible);
+    const userToggledMenu = await selectSnapshot(this._userToggledMenu);
+    
+    this._menuVisible.next(!menuVisible);
+    this._userToggledMenu.next(!userToggledMenu);
+  }
 
   @HostListener('window:resize', ['$event'])
-  onResize(event: Event) {
+  async onResize(event: Event) {
     this.visualizerComponentService.triggerResizeEvent();
-    this.validateClient();
+    await this.validateClient();
   }
 
   @HostListener('document:keypress', ['$event'])
-  onKeydown = (event: KeyboardEvent) => this.visualizerComponentService.keydown(event);
+  onKeydown = (event: KeyboardEvent) =>
+    this.visualizerComponentService.keydown(event);
 
   selectedGoodEdgeColor = selectedGoodEdgeColor;
 
-  validateClient() {
-    this._displayDetails.next(window.innerWidth >= 1000);
-    this._menuVisible.next(window.innerWidth >= 1000);
+  async validateClient() {
+    const menuVisible = await selectSnapshot(this._menuVisible);
+    const userToggledMenu = await selectSnapshot(this._userToggledMenu);
+    const wideView = window.innerWidth >= 1000;
+
+    this._displayDetails.next(wideView);
+    this._menuVisible.next(wideView && !userToggledMenu);
   }
 }
