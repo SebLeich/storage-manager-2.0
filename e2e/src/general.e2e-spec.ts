78d1e315b0bbdfb4314965c1376f854c0fb56893
Sebastian Leichsenring <sebleichsenring@gmail.com>
2022-10-02 01:53:41 +0200

    fixed UI test

--- e2e/src/general.e2e-spec.ts
+++ e2e/src/general.e2e-spec.ts
@@ -1,7 +1,6 @@
 import { HelperMethods } from './general-e2e.helper';
 import { browser, logging } from 'protractor';
-import { timer } from 'rxjs';
-import { selectSnapshot } from 'src/lib/process-builder/globals/select-snapshot';
+import { firstValueFrom, timer } from 'rxjs';
 
 describe('Storage Manager App: general e2e tests', () => {
   let page: HelperMethods = new HelperMethods();
@@ -16,7 +15,7 @@ describe('Storage Manager App: general e2e tests', () => {
 
     it(`should display ${mainView.component} component at url '${browser.baseUrl}${mainView.url}'`, async () => {
       await browser.waitForAngular();
-      await selectSnapshot(timer(1000));
+      await firstValueFrom(timer(1000));
       await page.navigateTo(`${browser.baseUrl}${mainView.url}`);
 
       await browser.waitForAngular();
