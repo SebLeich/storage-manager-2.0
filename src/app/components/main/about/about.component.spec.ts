631be52a3303f1a9085aba8f03a17185248d334c
Seb <sebleichsenring@gmail.com>
2022-10-04 21:35:13 +0200

    fixed unit test

--- src/app/components/main/about/about.component.spec.ts
+++ src/app/components/main/about/about.component.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import defaultImportsConstant from 'src/app/default-imports.constant';
 
 import { AboutComponent } from './about.component';
 
@@ -8,7 +9,10 @@ describe('AboutComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ AboutComponent ]
+      declarations: [ AboutComponent ],
+      imports: [
+        ...defaultImportsConstant
+      ]
     })
     .compileComponents();
 
