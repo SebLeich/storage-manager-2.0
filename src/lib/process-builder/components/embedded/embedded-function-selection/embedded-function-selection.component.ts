22c49a4b03edd800ad5a29872e07508075e294d1
SebLeich <sebleichsenring@gmail.com>
2023-02-03 01:55:52 +0100

    added filter option to function selection

--- src/lib/process-builder/components/embedded/embedded-function-selection/embedded-function-selection.component.ts
+++ src/lib/process-builder/components/embedded/embedded-function-selection/embedded-function-selection.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnDestroy, OnInit, QueryList, ViewChildren, ViewContainerRef } from '@angular/core';
+import { Component, ElementRef, Input, OnDestroy, OnInit, QueryList, ViewChild, ViewChildren, ViewContainerRef } from '@angular/core';
 import { ReplaySubject, Subscription } from 'rxjs';
 import { ParamCodes } from 'src/config/param-codes';
 import { EmbeddedView } from 'src/lib/process-builder/classes/embedded-view';
@@ -7,14 +7,15 @@ import { showAnimation } from 'src/lib/shared/animations/show';
 import { Store } from '@ngrx/store';
 import { selectIFunctions } from 'src/lib/process-builder/store/selectors/function.selector';
 import { FunctionPreviewComponent } from '../../previews/function-preview/function-preview.component';
-import { ControlContainer, FormGroup } from '@angular/forms';
+import { ControlContainer, FormControl, FormGroup } from '@angular/forms';
 import { IInputParam } from 'src/lib/process-builder/globals/i-input-param';
-import { delay, map } from 'rxjs/operators';
+import { delay, map, startWith } from 'rxjs/operators';
 
 import * as fromIFunctionState from 'src/lib/process-builder/store/reducers/function.reducer';
 import { removeIFunction } from 'src/lib/process-builder/store/actions/function.actions';
 import { showListAnimation } from 'src/lib/shared/animations/show-list';
 import { ITaskCreationFormGroup } from 'src/lib/process-builder/interfaces/i-task-creation.interface';
+import { combineLatest } from 'rxjs/internal/observable/combineLatest';
 
 @Component({
   selector: 'app-embedded-function-selection',
@@ -27,13 +28,44 @@ export class EmbeddedFunctionSelectionComponent implements EmbeddedView, OnDestr
   @Input() public inputParams!: ParamCodes | ParamCodes[] | null;
 
   @ViewChildren(FunctionPreviewComponent, { read: ViewContainerRef }) private activeFunctionWrappers!: QueryList<ViewContainerRef>;
+  @ViewChild('searchInput', { static: true, read: ElementRef }) private _searchInput!: ElementRef<HTMLInputElement>;
+
+  public filterControl = new FormControl<string>('');
+  private _filter$ = this.filterControl.valueChanges.pipe(startWith(''), map(filter => {
+    const transformed = (filter ?? '').trim().toLowerCase().split(' ').filter(segment => segment);
+    return transformed;
+  }));
 
   public get formGroup(): FormGroup<Partial<ITaskCreationFormGroup>> {
     return this._controlContainer.control as FormGroup<Partial<ITaskCreationFormGroup>>;
   }
 
+  public searchinputExpanded = false;
+
   private _availableFunctions = new ReplaySubject<IFunction[]>(1);
   public availableFunctions$ = this._availableFunctions.asObservable();
+
+  public functions$ = combineLatest([this._store.select(selectIFunctions()), this._filter$]).pipe(map(([functions, filter]) => {
+    return functions.filter(func => {
+      if (!func) {
+        return false;
+      }
+
+      if(filter.length > 0){
+        const functionNameSegments = func.name.trim().toLowerCase().split(' ')
+        const isNotMatchingFilter = filter.some(segment => functionNameSegments.every(functionSegment => functionSegment.indexOf(segment) === -1));
+        if(isNotMatchingFilter) return false;
+      }
+
+      const requiredInputs: ParamCodes[] = (Array.isArray(func.inputParams) ? func.inputParams : [func.inputParams]).filter(y => y && typeof y === 'object' && !y.optional).map((x) => (x as IInputParam).param) as ParamCodes[];
+      if (requiredInputs.length === 0) {
+        return true;
+      }
+
+      let availableInputParams: ParamCodes[] = Array.isArray(this.inputParams) ? this.inputParams : this.inputParams ? [this.inputParams] : [];
+      return true;
+    });
+  }));
 
   private _subscription: Subscription = new Subscription();
 
@@ -74,6 +106,11 @@ export class EmbeddedFunctionSelectionComponent implements EmbeddedView, OnDestr
   public selectFunction(func: IFunction) {
     const functionIdentifier = this.formGroup.controls.functionIdentifier!.value === func.identifier ? null : func.identifier;
     this.formGroup.controls.functionIdentifier!.setValue(functionIdentifier);
+  }
+
+  public toggleSearchInput(expand?: boolean) {
+    this.searchinputExpanded = typeof expand === 'boolean' ? expand : !this.searchinputExpanded;
+    this._searchInput.nativeElement[this.searchinputExpanded ? 'focus' : 'blur']();
   }
 
 }
