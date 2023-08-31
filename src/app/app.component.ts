fc937bd68b7938963b7a6ef5eca7540432a47721
Sebastian Leichsenring <Sebastian.Leichsenri@aitag.com>
2023-08-31 19:10:27 +0200

    added default models to initially set bpmn models

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,36 +4,41 @@ import { NavigationEnd, NavigationSkipped, NavigationStart, Router } from '@angu
 import { Store } from '@ngrx/store';
 import { filter, map, shareReplay } from 'rxjs';
 import { selectGlobalProcedureProgress, selectHasDeterminingProcedures, selectHasPendingProcedures } from '@procedure';
+import defaultModels from '@/config/default-models.constant';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.css'],
 })
 export class AppComponent implements OnInit {
-  public loadingRoute$ = this._router.events.pipe(
-    filter(event => event instanceof NavigationStart || event instanceof NavigationEnd || event instanceof NavigationSkipped),
-    map(event => event instanceof NavigationStart),
-    shareReplay(1)
-  );
+	public loadingRoute$ = this._router.events.pipe(
+		filter(event => event instanceof NavigationStart || event instanceof NavigationEnd || event instanceof NavigationSkipped),
+		map(event => event instanceof NavigationStart),
+		shareReplay(1)
+	);
 
-  public canProvideDeterminateProgress$ = this._store.select(selectHasDeterminingProcedures);
-  public globalProcedureProgress$ = this._store.select(selectGlobalProcedureProgress);
-  public hasPendingTasks$ = this._store.select(selectHasPendingProcedures);
+	public canProvideDeterminateProgress$ = this._store.select(selectHasDeterminingProcedures);
+	public globalProcedureProgress$ = this._store.select(selectGlobalProcedureProgress);
+	public hasPendingTasks$ = this._store.select(selectHasPendingProcedures);
 
-  constructor(
-    private _snackBar: MatSnackBar,
-    private _router: Router,
-    private _store: Store
-  ) { }
+	constructor(
+		private _snackBar: MatSnackBar,
+		private _router: Router,
+		private _store: Store
+	) { }
 
-  public ngOnInit(): void {
-    if (localStorage.getItem('cookie_consent') !== 'true') {
-      this._snackBar.open(
-        'In addition to cookies which are necessary for the proper functioning of websites, we use cookies on your browser to improve your experience.', 'Consent', { horizontalPosition: 'right', verticalPosition: 'top' })
-        .afterDismissed()
-        .subscribe(() => localStorage.setItem('cookie_consent', 'true'));
-    }
-  }
+	public ngOnInit(): void {
+		if (localStorage.getItem('cookie_consent') !== 'true') {
+			this._snackBar.open(
+				'In addition to cookies which are necessary for the proper functioning of websites, we use cookies on your browser to improve your experience.', 'Consent', { horizontalPosition: 'right', verticalPosition: 'top' })
+				.afterDismissed()
+				.subscribe(() => localStorage.setItem('cookie_consent', 'true'));
+		}
+
+		if (localStorage.getItem('models') == null) {
+			localStorage.setItem('models', JSON.stringify(defaultModels));
+		}
+	}
 
 }
