5a21415c951d882364728855be65c496de935e57
Sebastian Leichsenring <sebleichsenring@gmail.com>
2024-04-29 20:47:39 +0200

    enhancements

--- projects/storage-manager/src/app/app.component.ts
+++ projects/storage-manager/src/app/app.component.ts
@@ -9,7 +9,7 @@ import defaultModels from '@/config/default-models.constant';
 @Component({
 	selector: 'app-root',
 	templateUrl: './app.component.html',
-	styleUrls: ['./app.component.css'],
+	styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
 	public loadingRoute$ = this._router.events.pipe(
