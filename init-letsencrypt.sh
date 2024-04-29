89ca935693baa1cf5d48746c7edc31171c130d1f
Sebastian Leichsenring <sebleichsenring@gmail.com>
2024-04-30 01:52:13 +0200

    final fix

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -9,7 +9,7 @@ domains=(sebleich.de www.sebleich.de)
 rsa_key_size=4096
 data_path="./data/certbot"
 email="sebleichsenring@gmail.com"
-staging=1 # Set to 1 if you're testing your setup to avoid hitting request limits
+staging=0 # Set to 1 if you're testing your setup to avoid hitting request limits
 
 if [ -d "$data_path" ]; then
   read -p "Existing data found for $domains. Continue and replace existing certificate? (y/N) " decision
